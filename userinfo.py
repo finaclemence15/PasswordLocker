199d3742bfcf4ce70fa45cfbde3b20ab7d3b7f80
finaclemence15 <finaclemence15@gmail.com>
2019-09-05 13:07:27 +0200

    import Userinfo class

--- userinfo.py
+++ userinfo.py
@@ -1,13 +1,17 @@
 class Userinfo:
-        """
-        Class that generates new instances of Userinfo
-        """
-
-        def __init__(self, user_name, password):
-            '''
+    """
+    Class that generates new instances of Userinfo
+    """    
+    def __init__(self, user_name, password):
+        '''
         __init__ method that helps us define properties for our objects.
 
         Args:
             user_name: New userinfo user name.
             password : New userinfo password.
         '''
+
+        self.user_name = user_name
+        self.password = password
+        
+    users_list = [] #empty users list    
