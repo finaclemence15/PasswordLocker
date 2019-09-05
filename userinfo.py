ce390c7103d3d4ec62fbf75cc73d42a6f2aa7956
finaclemence15 <finaclemence15@gmail.com>
2019-09-05 12:40:19 +0200

    creation of class userinfo

---
+++ userinfo.py
@@ -0,0 +1,13 @@
+class Userinfo:
+        """
+        Class that generates new instances of Userinfo
+        """
+
+        def __init__(self, user_name, password):
+            '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            user_name: New userinfo user name.
+            password : New userinfo password.
+        '''
