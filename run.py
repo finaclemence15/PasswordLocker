28c57577e073e2f11ae943027a487c27acc86fd5
finaclemence15 <finaclemence15@gmail.com>
2019-09-06 17:29:01 +0200

    create new user

--- run.py
+++ run.py
@@ -37,3 +37,58 @@ def save_credential(credential):
     Function to save credential
     '''
     credential.save_credential()
+
+def del_credential(credential):
+    '''
+    Function to delete a credential
+    '''
+    credential.delete_credential()
+    
+
+def find_credential(username):
+    '''
+    Function that finds a credential by username and returns the credential
+    '''
+    return Credential.find_by_username(username)
+
+
+def display_credential():
+    '''
+    Function that returns all the saved credentials
+    '''
+    return Credential.display_credential()
+
+def main():
+    print("Welcome!")
+
+
+
+    print("Please enter one of the following codes to proceed, cc - create a new account, log -if you already have an account, ex - to exit the application ")
+    short_code = input().lower()
+
+    if short_code == 'cc':
+        print("Create new account")
+        print("Username")
+        user_name=input()
+        print("password")
+        password=input()
+
+
+        save_userinfo(create_userinfo(user_name,password))
+        print('\n')
+        print(f"Log in details for {user_name}  have been saved")
+        print('\n')
+
+        print("Please exit the application to log in to see your credentials")
+
+    elif short_code == 'log':
+
+        print("Fill in the required details")
+        print("Enter your user name")
+        user_name=input()
+        print("Enter your password")
+        password=input()
+
+    else:
+        print("User does not exist please create an account first")        
+    
