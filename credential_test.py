6383ec60b324c11733bad9286df85bedada35090
finaclemence15 <finaclemence15@gmail.com>
2019-09-06 12:25:34 +0200

    first test of credential

---
+++ credential_test.py
@@ -0,0 +1,40 @@
+import unittest 
+from credential import Credential
+
+class TestCredential(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the Credential class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    
+    
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_credential = Credential("James","facebook","jk","abcd") # create contact object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_credential.username,"James")
+        self.assertEqual(self.new_credential.cred_application,"facebook")
+        self.assertEqual(self.new_credential.cred_username,"jk")
+        self.assertEqual(self.new_credential.cred_password,"abcd")
+        
+# setup and class creation up here
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            Credential.credential_list = []
+           
+         
+if __name__ == '__main__':
+    unittest.main()        
